3dc62ebec6d27c64409397d00604d33ec6ff577b
Kenshin <517701884@qq.com>
2018-09-18 17:06:39 +0800

    ktpl

--- js/KTpl/KTpl.js
+++ js/KTpl/KTpl.js
@@ -19,29 +19,35 @@ KTpl.prototype = {
     o2s.forEach(function(item){
       item.innerHTML = this.convert(o1.innerHTML, d);
     }.bind(this));
-  }
+  },
 
   convert: function(s, d){
-    var str = "var s = '';";
+    try{
+      var str = "var s = '';";
 
-    var s_arr = s.split(this.__LeftTag);
-    str += "s += '" + s_arr[0] + "';";
-    if(s_arr.length <= 1) return str;
-    for(var i = 1; i < s_arr.length; i++){
-      var s_r_arr = s_arr[i].split(this.__RightTag);
-      if(s_r_arr[0][0] == this.__EqualTag){
-        str += "s += " + s_r_arr[0].substring(1) + ";";
-      }
-      else{
-        str += s_r_arr[0] + ";";
+      var s_arr = s.split(this.__LeftTag);
+      str += "s += '" + s_arr[0] + "';";
+      if(s_arr.length <= 1) return str;
+      for(var i = 1; i < s_arr.length; i++){
+        var s_r_arr = s_arr[i].split(this.__RightTag);
+        if(s_r_arr[0][0] == this.__EqualTag){
+          str += "s += " + s_r_arr[0].substring(1) + ";";
+        }
+        else{
+          str += s_r_arr[0] + ";";
+        }
+
+        str += "s += '" + s_r_arr[1] + "';";
+        str = str.replace(/\r\n/g, '\\n').replace(/\n/g, '\\n').replace(/\r/g, '\\n');
       }
 
-      str += "s += '" + s_r_arr[1] + "';";
-      str = str.replace(/\r\n/g, '\\n').replace(/\n/g, '\\n').replace(/\r/g, '\\n');
+      str += 'return s;'
+      return this.makeFunction(str,d);
     }
-
-    str += 'return s;'
-    return this.makeFunction(str,d);
+    catch(e){
+      console.log(e);
+      return e;
+    }
   },
 
   makeFunction: function(s, d){
