8a7d8ddb3e78647e1bbae25584faa2b5ccc7c97b
Kenshin <517701884@qq.com>
2019-02-02 13:37:38 +0800

    corsAjax

--- php/middle/CorsAjax.php
+++ php/middle/CorsAjax.php
@@ -8,12 +8,12 @@ class CorsAjax
 {
   public function handle($request, Closure $next)
   {
-    header('Access-Control-Allow-Origin: *');
+    $origin = $request->server('HTTP_ORIGIN');
+    header('Access-Control-Allow-Origin: '. $origin);
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: *");
     header("Access-Control-Allow-Headers: Content-Type,Access-Token");
     header("Access-Control-Expose-Headers: *");
-
     return $next($request);
   }
 }
