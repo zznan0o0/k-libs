429b08c9bf77496a01a7d4dfb40c10652aed5b4f
Kenshin <517701884@qq.com>
2019-04-12 17:06:04 +0800

    py

--- python/KHandleData.py
+++ python/KHandleData.py
@@ -1,12 +1,37 @@
 class KHandleData:
+  def distributePropsArray(self, d1, d2, k1, k2, p):
+    def fn(v1, v2, k):
+      for pv in p:
+        v1[pv[0]] = self.getVal(v2, pv[1], pv[2])
+      
+      return v1
+
+    return self.mapDictDict(d1, d2, k1, k2, fn)
+
+
   def mapDictDict(self, d1, d2, k1, k2, fn):
     dict2 = self.convertDict(d2, k2)
     arr = []
     for v in d1:
       key = self.convertKey(v, k1)
-      arr.append(fn(v, self.getVal(dict2, key, []), key))
+      arr.append(fn(v, self.getVal(dict2, key, {}), key))
     
     return arr
+  
+  def mapDicts(self, d, key, fn):
+    dic = self.convertDicts(d, key)
+    arr = []
+    for k, v in dic.items():
+      arr.append(fn(v, k))
+    return arr
+  
+  def convertDicts(self, d, k):
+    dic = {}
+    for v in d:
+      key = self.convertKey(v, k)
+      dic = self.setVals(dic, key, v)
+    
+    return dic
 
   def convertDict(self, d, k):
     dic = {}
@@ -22,9 +47,23 @@ class KHandleData:
       arr.append(str(self.getVal(d, kv, '')))
 
     return ','.join(arr)
+  
+  def setVals(self, d, k, v):
+    if k not in d.keys():
+      d[k] = []
+    
+    d[k].append(v)
+    return d
+
+  def getVals(self, d, k, intval=''):
+    arr = []
+    for v in d:
+      arr.append(self.getVal(v, k, intval))
+    return arr
 
   def getVal(self, d, k, intval = ''):
     return intval if k not in d.keys() else d[k]
+  
   
   def getDictsVal(self, d, k, intval = ''):
     arr = []
@@ -36,13 +75,15 @@ class KHandleData:
 if __name__ == '__main__':
   kHandleData = KHandleData()
 
-  d1 = [{'k': 1, 'v':2}, {'k': 2, 'v':3}]
+  d1 = [{'k': "a", 'v':2}, {'k': "b", 'v':3}, {'k': "b", 'v': 24}]
   d2 = [{'k': 1, 'c':3}, {'k': 2, 'c':4}]
-  c = []
-  def aaa(v1, v2, key):
-    v1['c'] = kHandleData.getVal(v2, 'c', '')
-    c.append(v1)
-    return v1
 
-  bbb = kHandleData.mapDictDict(d1, d2, ['k'], ['k'], aaa)
+  def fn(v, k):
+    for vv in v:
+      vv['ccc'] = 0
+    
+    return v
+    
+  c = kHandleData.mapDicts(d1, ['k'], fn)
+  
   print(c)
